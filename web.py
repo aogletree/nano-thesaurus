37a2f94ac2d839b2b836561b109c62cbd7617422
Adrian Ogletree <adrianogletree@gmail.com>
2015-05-28 08:30:03 -0400

    Working on stemming

--- web.py
+++ web.py
@@ -90,7 +90,19 @@ def eliminate_words(in_list):
                 is_not_ignored = False
         if(is_not_ignored):
             out.append(x)
-    return out
+            
+    # filter a second time
+    out2=[]
+    base_ignore_words=['magnet','propert']
+    for i in out:
+        is_not_ignored=True
+        for j in base_ignore_words:
+            if (j in i):
+                is_not_ignored= False
+        if(is_not_ignored):
+            out2.append(i)
+        #check to see if the word is derivative of another
+    return out2
 
 def main():
     """
